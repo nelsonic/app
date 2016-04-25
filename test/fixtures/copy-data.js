d96e5e949d5128c08272c3e6c81ac0dd42a760d9
Anita <a.nita@hotmail.co.uk>
2016-04-25 14:44:09 +0100

    replaced MAP_ID_USERS on clients,jobs,dahsboard endpoints see https://github.com/FAC-GM/app/issues/419#issuecomment-214333468

--- test/fixtures/copy-data.js
+++ test/fixtures/copy-data.js
@@ -1,3 +1,4 @@
+'use strict';
 require('env2')('.env');
 
 var client = require('./elastic.js')(process.env.SEARCHBOX_URL_FIXTURES);
@@ -8,7 +9,7 @@ var count = 0;
 client.search({
 
   index: process.env.ES_INDEX_FIXTURES,
-  type: process.env.ES_TYPE_USERS,
+  type: process.env.ES_TYPE_CURRENT_USERS,
   scroll: '30s',
   size: 1000,
   search_type: 'scan'
@@ -18,18 +19,38 @@ client.search({
   if(error) {
     console.log('error', error);
   }
-
   var bulk = [];
   response.hits.hits.forEach(function (e) {
     // console.log(e._source);
-    bulk.push({index: {_index: 'gmcontact', _type: process.env.ES_TYPE_USERS, _id: e._id}})
-    bulk.push(e._source);
+    if (e._source.names) {
+      var newData = {};
+      newData.email = e._source.email;
+      newData.idGoogle = e._id;
+      newData.image = '';
+      newData.idWebsite = e._id;
+      newData.names = {
+        firstname: e._source.names.firstname,
+        fullname: e._source.names.fullname,
+        lastname: e._source.names.lastname,
+        linkedinName: e._source.names.linkedinName
+      };
+      newData.phones = {
+        office: e._source.phones.office,
+        mobile: e._source.phonesmobile
+      };
+      newData.role = e._source.role;
+      newData.linkedin = e._source.linkedin;
+
+
+      bulk.push({index: {_index: 'gmcontact', _type: process.env.ES_TYPE_GM_USERS, _id: e._id}})
+      bulk.push(newData);
+    }
 
     count += 1;
   })
 
   clientLocal.bulk({ body: bulk}, function (err, resp) {
-
+    console.log('resp',resp);
     if (response.hits.total !== count) {
     // now we can call scroll over and over
       client.scroll({
