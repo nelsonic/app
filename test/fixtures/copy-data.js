7e16f609cbc075365e213dddabb1f56371a87e54
SimonLab <simon.labondance@gmail.com>
2016-05-03 12:59:45 +0100

    get the new structure of the users for the tests, #419

--- test/fixtures/copy-data.js
+++ test/fixtures/copy-data.js
@@ -9,7 +9,7 @@ var count = 0;
 client.search({
 
   index: process.env.ES_INDEX_FIXTURES,
-  type: process.env.ES_TYPE_CURRENT_USERS,
+  type: process.env.ES_TYPE_GM_USERS,
   scroll: '30s',
   size: 1000,
   search_type: 'scan'
@@ -21,32 +21,9 @@ client.search({
   }
   var bulk = [];
   response.hits.hits.forEach(function (e) {
-    // console.log(e._source);
-    if (e._source.names) {
-      var newData = {
-        email: e._source.email,
-        idGoogle: e._id,
-        image: '',
-        idWebsite: e._id,
-        names: {
-          firstname: e._source.names.firstname,
-          fullname: e._source.names.fullname,
-          lastname: e._source.names.lastname,
-          linkedinName: e._source.names.linkedinName
-        },
-        phones: {
-          office: e._source.phones.office,
-          mobile: e._source.phones.mobile
-        },
-        role: e._source.role,
-        linkedin: e._source.linkedin,
-        active: true,
-        admin: true
-      };
-    
-      bulk.push({index: {_index: 'gmcontact', _type: process.env.ES_TYPE_GM_USERS, _id: e._id}})
-      bulk.push(newData);
-    }
+
+    bulk.push({index: {_index: 'gmcontact', _type: process.env.ES_TYPE_GM_USERS, _id: e._id}})
+    bulk.push(e._source);
 
     count += 1;
   })
