62036800ce444ac427e5113175053bab1f3d4f59
Anita <a.nita@hotmail.co.uk>
2016-05-07 22:10:55 +0100

    added status to fixtures

--- test/status.test.js
+++ test/status.test.js
@@ -42,6 +42,7 @@ describe('submit the status when authenticated', function () {
     var status = {
       idCandidate: '88',
       idUser: '12',
+      idJob: '1',
       idStage: 'stage1',
       timestamp: '89898989',
       idClient: '1'
@@ -50,6 +51,64 @@ describe('submit the status when authenticated', function () {
     var options = {
       method: "POST",
       url: "/status/save",
+      headers: { cookie: "token=" + token },
+      credentials: { id: "12", "name": "Simon", valid: true},
+      payload: status
+    };
+
+    Server.init(0, function (err, server) {
+
+      expect(err).to.not.exist();
+
+      server.inject(options, function (res) {
+
+        expect(res.statusCode).to.equal(302);
+
+        server.stop(done);
+      });
+    });
+  });
+});
+
+describe('/status/edit', function () {
+
+  it('attempt to edit status without being authenticated', function (done) {
+
+    var options = {
+      method: "POST",
+      url: "/status/edit"
+    };
+
+    Server.init(0, function (err, server) {
+
+      expect(err).to.not.exist();
+
+      server.inject(options, function (res) {
+
+        expect(res.statusCode).to.equal(302);
+
+        server.stop(done);
+      });
+    });
+  });
+});
+
+describe('edit the status when authenticated', function () {
+
+  it('redirects to the candidate page with new edited status', function (done) {
+
+    var status = {
+      idCandidate: '88',
+      timestamp: '89898989',
+      idJob: '1',
+      idStage: 'stage4',
+      idClient: '1',
+      idUser: '12'
+    };
+
+    var options = {
+      method: "POST",
+      url: "/status/edit",
       headers: { cookie: "token=" + token },
       credentials: { id: "12", "name": "Simon", valid: true},
       payload: status
@@ -104,63 +163,6 @@ describe('delete the status when authenticated', function () {
     var options = {
       method: "POST",
       url: "/status/delete",
-      headers: { cookie: "token=" + token },
-      credentials: { id: "12", "name": "Simon", valid: true},
-      payload: status
-    };
-
-    Server.init(0, function (err, server) {
-
-      expect(err).to.not.exist();
-
-      server.inject(options, function (res) {
-
-        expect(res.statusCode).to.equal(302);
-
-        server.stop(done);
-      });
-    });
-  });
-});
-
-describe('/status/edit', function () {
-
-  it('attempt to edit status without being authenticated', function (done) {
-
-    var options = {
-      method: "POST",
-      url: "/status/edit"
-    };
-
-    Server.init(0, function (err, server) {
-
-      expect(err).to.not.exist();
-
-      server.inject(options, function (res) {
-
-        expect(res.statusCode).to.equal(302);
-
-        server.stop(done);
-      });
-    });
-  });
-});
-
-describe('edit the status when authenticated', function () {
-
-  it('redirects to the candidate page with new edited status', function (done) {
-
-    var status = {
-      idCandidate: '88',
-      timestamp: '67676767',
-      idJob: '1',
-      idStage: 'stage1',
-      idClient: '1'
-    };
-
-    var options = {
-      method: "POST",
-      url: "/status/edit",
       headers: { cookie: "token=" + token },
       credentials: { id: "12", "name": "Simon", valid: true},
       payload: status
