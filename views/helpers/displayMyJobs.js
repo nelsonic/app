5bac9995ca81441b4c14c709208e822d59abbf92
SimonLab <simon.labondance@gmail.com>
2016-05-10 20:47:30 +0100

    add delete functionality, #423

--- views/helpers/displayMyJobs.js
+++ views/helpers/displayMyJobs.js
@@ -18,7 +18,14 @@ module.exports = function (stages, idJob, jobsDetail, stagesDetail) {
     result += '<div class="candidate stage-title">' + stagesDetail[idStage].name +'</div>';
     let candidates = stages[idStage];
     candidates.forEach(candidate => {
-      result += '<div class="candidate"><button>x</button><img src=' + candidate.picture +' /><p>' + candidate.fullname +'</p></div>'
+      result += '<div class="candidate">';
+      result += '<form class="delete-status-form" action="/status/delete" method="POST">';
+      result += '<input type="hidden" name="idCandidate" value="' + candidate.id +'">';
+      result += '<input type="hidden" name="timestamp" value="' + candidate.statusTimestamp + '">';
+      result += '<input type="hidden" name="redirectDashboard" value="true">'
+      result += '<button>x</button>'
+      result += '</form>'
+      result += '<img src=' + candidate.picture +' /><p>' + candidate.fullname +'</p></div>'
     })
     result += '</div>'
   }
