a310222b7d0924a46849fe6e91fe2f2e526012b0
SimonLab <simon.labondance@gmail.com>
2016-06-06 09:56:37 +0100

    more tests for #475

--- test/client_dashboard.test.js
+++ test/client_dashboard.test.js
@@ -9,26 +9,26 @@ var describe = lab.experiment;
 var expect = Code.expect;
 var it = lab.test;
 
-//
-// describe('/client-dashboard', function () {
-//
-//   it('Attempt to access the client dashboard without authentication', function (done) {
-//
-//     Server.init(0, function (err, server) {
-//
-//       expect(err).to.not.exist();
-//
-//       server.inject('/client-dashboard', function (res) {
-//
-//         //redirect to the client login page
-//         expect(res.statusCode).to.equal(302);
-//         expect(res.headers.location).to.equal('/client-login')
-//         server.stop(done);
-//       });
-//     });
-//   });
-// });
-//
+
+describe('/client-dashboard', function () {
+
+  it('Attempt to access the client dashboard without authentication', function (done) {
+
+    Server.init(0, function (err, server) {
+
+      expect(err).to.not.exist();
+
+      server.inject('/client-dashboard', function (res) {
+
+        //redirect to the client login page
+        expect(res.statusCode).to.equal(302);
+        expect(res.headers.location).to.equal('/client-login')
+        server.stop(done);
+      });
+    });
+  });
+});
+
 describe('Access /client-dashboard with authorization', function () {
 
   it('return dashboard for the client', function (done) {
@@ -39,19 +39,17 @@ describe('Access /client-dashboard with authorization', function () {
       expect(err).to.not.exist();
       //create the client in redis
       var redisClient = require('redis-connection')();
-      redisClient.set(4242, JSON.stringify({email: 'theemail@email.com', authorized: true}), function(errRedis, responseRedis){
+      //you need to provide idClient to be able to use it on request.auth.credentials.idClient
+      redisClient.set(4242, JSON.stringify({email: 'theemail@email.com', authorized: true, idClient: '1'}), function(errRedis, responseRedis){
 
         var options = {
           method: "GET",
           url: "/client-dashboard",
           headers: { cookie: "token_client=" + token_client },
-          credentials: {idClient: '1'}
         };
 
         server.inject(options, function (res) {
 
-          console.log('access the dashoboard!');
-          console.log(res.payload);
           expect(res.statusCode).to.equal(200);
 
           server.stop(done);
@@ -62,60 +60,63 @@ describe('Access /client-dashboard with authorization', function () {
     });
   });
 });
-//
-// describe('Access /client-dashboard with authorization to false', function () {
-//
-//   it('return to the login page', function (done) {
-//
-//     var token_client =  JWT.sign({ sid: 4242}, process.env.JWT_SECRET_CLIENT);
-//     Server.init(0, function (err, server) {
-//
-//       expect(err).to.not.exist();
-//       //create the client in redis
-//       var redisClient = require('redis-connection')();
-//       redisClient.set(4242, JSON.stringify({email: 'theemail@email.com', authorized: false}), function(errRedis, responseRedis){
-//
-//         var options = {
-//           method: "GET",
-//           url: "/client-dashboard",
-//           headers: { cookie: "token_client=" + token_client },
-//         };
-//
-//         server.inject(options, function (res) {
-//           //redirec to the login page
-//           expect(res.statusCode).to.equal(302);
-//
-//           server.stop(done);
-//         });
-//
-//       })
-//
-//     });
-//   });
-// });
-//
-// describe('Access /client-dashboard without client found in redis', function () {
-//
-//   it('Redirect to the login page', function (done) {
-//
-//     var token_client =  JWT.sign({ sid: 424233333}, process.env.JWT_SECRET_CLIENT);
-//     Server.init(0, function (err, server) {
-//
-//       expect(err).to.not.exist();
-//
-//         var options = {
-//           method: "GET",
-//           url: "/client-dashboard",
-//           headers: { cookie: "token_client=" + token_client },
-//         };
-//
-//         server.inject(options, function (res) {
-//           //redirect to the login page
-//           expect(res.statusCode).to.equal(302);
-//
-//           server.stop(done);
-//         });
-//
-//     });
-//   });
-// });
+
+describe('Access /client-dashboard with authorization to false', function () {
+
+  it('return to the login page', function (done) {
+
+    var token_client =  JWT.sign({ sid: 4242}, process.env.JWT_SECRET_CLIENT);
+    Server.init(0, function (err, server) {
+
+      expect(err).to.not.exist();
+      //create the client in redis
+      var redisClient = require('redis-connection')();
+      redisClient.set(4242, JSON.stringify({email: 'theemail@email.com', authorized: false}), function(errRedis, responseRedis){
+
+        var options = {
+          method: "GET",
+          url: "/client-dashboard",
+          headers: { cookie: "token_client=" + token_client },
+        };
+
+        server.inject(options, function (res) {
+          //redirec to the login page
+          expect(res.statusCode).to.equal(302);
+
+          server.stop(done);
+        });
+
+      })
+
+    });
+  });
+});
+
+describe('Access /client-dashboard without client found in redis', function () {
+
+  it('Redirect to the login page', function (done) {
+
+    var token_client =  JWT.sign({ sid: 424233333}, process.env.JWT_SECRET_CLIENT);
+    Server.init(0, function (err, server) {
+
+      expect(err).to.not.exist();
+      //create the client in redis
+      var redisClient = require('redis-connection')();
+
+        var options = {
+          method: "GET",
+          url: "/client-dashboard",
+          headers: { cookie: "token_client=" + token_client },
+        };
+
+        server.inject(options, function (res) {
+          //redirect to the login page
+          expect(res.statusCode).to.equal(302);
+
+          server.stop(done);
+        });
+
+
+    });
+  });
+});
